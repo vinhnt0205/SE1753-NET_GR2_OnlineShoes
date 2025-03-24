ef0c659516e965d6ab8faedcd35d9061a2e79881
hmanbr <tomhay2473@gmail.com>
2025-03-24 11:57:04 +0700

    more dashboard button

--- app/src/main/java/com/example/se1753net_gr2_onlineshoes/ui/activities/marketing_activities/MarketingDashboardActivity.java
+++ app/src/main/java/com/example/se1753net_gr2_onlineshoes/ui/activities/marketing_activities/MarketingDashboardActivity.java
@@ -12,13 +12,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.se1753net_gr2_onlineshoes.R;
+import com.example.se1753net_gr2_onlineshoes.activity.activity_login;
 import com.example.se1753net_gr2_onlineshoes.data.local.dao.ProductStatisticsDao;
 import com.example.se1753net_gr2_onlineshoes.data.local.database.ShoeShopDatabase;
 import com.example.se1753net_gr2_onlineshoes.data.local.entities.ProductStatistics;
+import com.example.se1753net_gr2_onlineshoes.ui.activities.sale_activities.OrderListActivity;
+import com.example.se1753net_gr2_onlineshoes.ui.activities.sale_activities.SaleDashboardActivity;
 
 public class MarketingDashboardActivity extends AppCompatActivity {
     private CardView productsCardView;
     private CardView analyticsCardView;
+    private CardView loginCardView;
+    private CardView salesCardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +51,26 @@ public class MarketingDashboardActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MarketingDashboardActivity.this, MarketingAnalyticActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        loginCardView = findViewById(R.id.loginCardView);
+
+        loginCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MarketingDashboardActivity.this, activity_login.class);
+                startActivity(intent);
+            }
+        });
+
+        salesCardView = findViewById(R.id.salesCardView);
+
+        salesCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MarketingDashboardActivity.this, SaleDashboardActivity.class);
                 startActivity(intent);
             }
         });
